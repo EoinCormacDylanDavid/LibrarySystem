b7d35cf60dcef7c4cf255186830575477b9d960b
EoinCormacDylanDavid <eddcsofttools@gmail.com>
2021-11-04 10:29:33 +0000

    controllers done 04/11/21

--- librarySystem/routes/index.js
+++ librarySystem/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const ctrlLocations = require('../controllers/locations');
+
+router.get('/', ctrlLocations.home);
+router.get('/viewBooks', ctrlLocations.viewBooks);
+router.get('/login', ctrlLocations.login);
+router.get('/register', ctrlLocations.register);
+
 
 module.exports = router;
