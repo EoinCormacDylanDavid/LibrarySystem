eef75942feccf9df3ed70f277f96643e2b5248de
EoinCormacDylanDavid <93678087+EoinCormacDylanDavid@users.noreply.github.com>
2021-11-09 11:35:30 +0000

    Update books.js

--- app_server/controllers/books.js
+++ app_server/controllers/books.js
@@ -5,7 +5,7 @@ const apiOptions = {
     server : 'http://localhost:3000' 
     }; 
     if (process.env.NODE_ENV === 'production') { 
-    apiOptions.server = 'https://pure-temple-67771.herokuapp.com'; //need to put our heroku link here
+    apiOptions.server = 'https://librarysystemeddc.herokuapp.com/'; //need to put our heroku link here
     }
 
     const displayAdminAddBook = function(req,res){
